885ce2d8129f1c667b9248df25d67ea26f3a6277
Anastasia <anastasiya.reksius@mail.ru>
2024-10-15 19:46:41 +0700

    add copy function

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -6,7 +6,7 @@ function createDeleteSection(taskDiv, deleteButton) {
         const deleteSection = document.querySelector('.delete-section');
         deleteSection.style.display = 'block';
     
-        document.querySelector('.no').addEventListener('click', (event) => {
+        document.querySelector('.no').addEventListener('click', () => {
             deleteSection.style.display = 'none';
         });
     
@@ -29,38 +29,42 @@ function createDeleteSection(taskDiv, deleteButton) {
     });
 }
 
+
 function createEditSection(taskDiv, taskTitle, taskDescription) {
     const editButton = taskDiv.querySelector('.edit-button');
     
-    editButton.addEventListener('click', (event) => {
-        event.stopPropagation();
+    editButton.addEventListener('click', () => {
         const editSection = document.querySelector('.edit-section');
         
         const currentTitle = taskTitle.textContent;
-        const currentDescription = taskDescription.textContent;
+        const currentDescription = taskDiv.getAttribute('data-full-description'); 
     
         const editTitleInput = document.querySelector('.title-element');
         const editDescriptionInput = document.querySelector('.description-element');
     
         editTitleInput.value = currentTitle;
-        editDescriptionInput.value = currentDescription;
+        editDescriptionInput.value = currentDescription; 
     
-        editSection.style.display = 'flex';
+        editSection.style.display = 'block';
     
         const saveButton = document.querySelector('.save');
         saveButton.onclick = () => {
             taskTitle.textContent = editTitleInput.value;
-            taskDescription.textContent = editDescriptionInput.value;
+
+            const newDescription = editDescriptionInput.value;
+            taskDescription.textContent = newDescription.length > 80 ? newDescription.substring(0, 80) + '...' : newDescription;
+            taskDiv.setAttribute('data-full-description', newDescription); 
     
             editSection.style.display = 'none';
     
             saveTasksToLocalStorage();
         };
+
         const cancelButton = document.querySelector('.cancel');
         cancelButton.onclick = () => {
             editSection.style.display = 'none';
         }
-    
+
         editSection.addEventListener('click', (event) => {
             if (!event.target.closest('.edit')) {
                 editSection.style.display = 'none';
@@ -69,10 +73,12 @@ function createEditSection(taskDiv, taskTitle, taskDescription) {
     });
 }
 
+
+
 function createShareSection(taskDiv) {
     const shareButton = taskDiv.querySelector('.share-button');
     
-    shareButton.addEventListener('click', (event) => {
+    shareButton.addEventListener('click', () => {
         const menuSection = document.querySelector('.share-section');
         menuSection.style.display = 'flex'; 
 
@@ -80,6 +86,19 @@ function createShareSection(taskDiv) {
             if (!event.target.closest('.menu')) {
                 menuSection.style.display = 'none';
             }
+        });
+    });
+
+    const copyButton = document.querySelector('.share-section .menu li img[src*="copy.svg"]');
+    copyButton.addEventListener('click', () => {
+        const taskTitle = taskDiv.querySelector('h3').textContent;
+        const taskDescription = taskDiv.querySelector('p').textContent;
+        const taskText = `${taskTitle}\n ${taskDescription}`;
+
+        navigator.clipboard.writeText(taskText).then(() => {
+            alert('Текст успешно скопирован');
+        }).catch(err => {
+            console.error('что-то пошло не так: ', err);
         });
     });
 }
