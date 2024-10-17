45a3be5c111c0bee674801bd782069cdf9fecf8f
Anastasia <anastasiya.reksius@mail.ru>
2024-10-17 19:08:45 +0700

    add separate script for sharing

---
+++ src/scripts/shareTask.js
@@ -0,0 +1,30 @@
+function createShareSection(taskDiv, title, description) {
+    const shareButton = taskDiv.querySelector('.share-button');
+    shareButton.addEventListener('click', () => {
+        const menuSection = document.querySelector('.share-section');
+        menuSection.style.display = 'flex'; 
+
+        document.querySelector('.share-section').addEventListener('click', (event) => {
+            if (!event.target.closest('.menu')) {
+                menuSection.style.display = 'none';
+            }
+        });
+
+        const copyButton = document.querySelector('.copy');
+        copyButton.addEventListener('click', () => {
+            copyText(title, description);
+        });
+    });   
+}
+
+function copyText(title, description) {
+    const taskTitle = title.textContent;
+    const taskDescription = description.textContent;
+    const taskText = `${taskTitle} ` + `${taskDescription}`;
+
+    navigator.clipboard.writeText(taskText).then(() => {
+        alert('Текст успешно скопирован');
+    }).catch(err => {
+        console.error('что-то пошло не так: ', err);
+    });
+}
