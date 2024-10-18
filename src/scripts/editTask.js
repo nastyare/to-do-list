c509aa787079f1f31b1a6098163a016b3c87a09b
Anastasia <anastasiya.reksius@mail.ru>
2024-10-18 23:09:56 +0700

    add script for creating edit container and its elements

--- src/scripts/editTask.js
+++ src/scripts/editTask.js
@@ -1,4 +1,40 @@
 function createEditSection(taskDiv, taskTitle, taskDescription) {
+    const editSectionDiv = document.createElement('div');
+    editSectionDiv.classList.add('edit-section', 'background');
+
+    const editSection = document.createElement('div');
+    editSection.classList.add('edit');
+
+    const editElements = document.createElement('div');
+    editElements.classList.add('edit-elements');
+
+    const titleElement = document.createElement('input');
+    titleElement.classList.add('title-element', 'edit-element');
+    const descriptionElement = document.createElement('textarea');
+    descriptionElement.classList.add('description-element', 'edit-element');
+
+    editElements.appendChild(titleElement);
+    editElements.appendChild(descriptionElement);
+
+    const buttonElements = document.createElement('div');
+    buttonElements.classList.add('buttons');
+
+    const cancelButton = document.createElement('button');
+    cancelButton.classList.add('cancel');
+    cancelButton.textContent = 'Cancel';
+    const saveButton = document.createElement('button');
+    saveButton.classList.add('save');
+    saveButton.textContent = 'Save';
+
+    buttonElements.appendChild(cancelButton);
+    buttonElements.appendChild(saveButton);
+
+    editSection.appendChild(editElements);
+    editSection.appendChild(buttonElements);
+    editSectionDiv.appendChild(editSection);
+
+    document.body.appendChild(editSectionDiv);
+
     const editButton = taskDiv.querySelector('.edit-button');
     
     editButton.addEventListener('click', () => {
