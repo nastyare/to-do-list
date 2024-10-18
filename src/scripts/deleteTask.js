1bd4df173bdd528b3de3d87d4141e9b5c07af1aa
Anastasia <anastasiya.reksius@mail.ru>
2024-10-18 23:10:29 +0700

    add code for creating delete container and its elements

--- src/scripts/deleteTask.js
+++ src/scripts/deleteTask.js
@@ -1,17 +1,37 @@
+let taskToDelete = null;
 
 function createDeleteSection(taskDiv, deleteButton) {
     deleteButton.addEventListener('click', (e) => {
         e.stopPropagation();
         taskToDelete = taskDiv;
-        
-        const deleteSection = document.querySelector('.delete-section');
-        deleteSection.style.display = 'block';
-        
-        document.querySelector('.no').addEventListener('click', () => {
-            deleteSection.style.display = 'none';
-        });
-        
-        document.querySelector('.yes').addEventListener('click', () => {
+
+        const deleteSectionDiv = document.createElement('div');
+        deleteSectionDiv.classList.add('delete-section', 'background');
+
+        const deleteWindow = document.createElement('div');
+        deleteWindow.classList.add('delete-window');
+
+        const confirmQuestion = document.createElement('p');
+        confirmQuestion.textContent = 'Delete this task?';
+
+        const yesButton = document.createElement('button');
+        yesButton.classList.add('yes');
+        yesButton.textContent = 'Yes';
+
+        const noButton = document.createElement('button');
+        noButton.classList.add('no');
+        noButton.textContent = 'No';
+
+
+        deleteWindow.appendChild(confirmQuestion);
+        deleteWindow.appendChild(yesButton);
+        deleteWindow.appendChild(noButton);
+        deleteSectionDiv.appendChild(deleteWindow);
+        document.body.appendChild(deleteSectionDiv);
+
+        deleteSectionDiv.style.display = 'block';
+
+        yesButton.addEventListener('click', () => {
             const taskId = taskDiv.getAttribute('id'); 
             deleteTaskFromLocalStorage(taskId); 
             tasksList.removeChild(taskDiv); 
@@ -20,14 +40,21 @@ function createDeleteSection(taskDiv, deleteButton) {
                 noTasksMessage.style.display = 'block'; 
             }
         
-            deleteSection.style.display = 'none'; 
+            deleteSectionDiv.style.display = 'none'; 
+        });
+
+        noButton.addEventListener('click', () => {
+            deleteSectionDiv.style.display = 'none';
         });
     
-        document.querySelector('.delete-section').addEventListener('click', (event) => {
+        deleteSectionDiv.addEventListener('click', (event) => {
             if (!event.target.closest('.delete-window')) {
-                deleteSection.style.display = 'none';
+                deleteSectionDiv.style.display = 'none';
             }
         });
     });
+
+
+    
 }
 
