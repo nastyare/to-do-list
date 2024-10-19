9f711827bbaa38ba5620d036951030b4ee83b1ab
Anastasia <anastasiya.reksius@mail.ru>
2024-10-19 19:08:36 +0700

    add all script for adding task

---
+++ src/scripts/addTask.js
@@ -0,0 +1,20 @@
+const { noTasks, tasksList } = createMainSection();
+
+function addTask(titleInput, descriptionInput) {
+    const title = titleInput.value.trim();
+    const description = descriptionInput.value.trim();
+
+    if (title && description) {
+        const taskId = Date.now(); 
+        createTaskItem(taskId, title, description);
+        saveTasksToLocalStorage(); 
+        titleInput.value = ''; 
+        descriptionInput.value = ''; 
+        noTasks.style.display = 'none'; 
+    } else {
+        alert("Должны быть заполнены и название, и описание");
+    }
+}
+
+
+
